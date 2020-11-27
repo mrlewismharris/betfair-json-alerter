6d87083d6f98d909382e935d69edc1bd6878957b
mrlewismharris <lewis_h_97@hotmail.co.uk>
2020-11-27 15:49:03 +0000

    updated to somewhat usable version
    added ifs to validate information is not empty

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const url = 'https://www.betfair.com/sport/inplay';
 //returned 1/100 = 0.01 (0.01=1% return)
-const returned = 0.5;
+const returnMax = 0.05;
 
 function loop() {
 	request(url, function(error, response, body) {
@@ -15,43 +15,40 @@ function loop() {
 		var out = [];
 		for (var i=0;i<inPlayGames.length;i++) {
 			var temp = [];
-			
 			//push url
-			console.log(inPlayGames[i]['parent']['parent']['parent']['parent']);
-			temp.push("https://www.betfair.com"+inPlayGames[i]['parent']['parent']['parent']['parent']);
-			//['prev']['prev']
-			//['attribs']['href']);
-			//push odds
-			var getOdds = inPlayGames[i]['children'][0]['data'];
-			temp.push(getOdds.replace(/\n/g, "").replace(/ /g, ""));
-			out.push(temp);
+			//console.log(inPlayGames[i]['parent']['parent']['parent']['parent']);
+			if (inPlayGames[i]['parent']['parent']['parent']['parent']['prev']['prev']) {
+				temp.push("https://www.betfair.com"+inPlayGames[i]['parent']['parent']['parent']['parent']['prev']['prev']['attribs']['href']);
+				//push odds
+				if (inPlayGames[i]['children'][0]['data'].trim()!=="") {
+					var getOdds = inPlayGames[i]['children'][0]['data'];
+					temp.push(getOdds.replace(/\n/g, "").replace(/ /g, ""));
+					out.push(temp);
+				}
+			}
 		}
-		
 		fs.writeFile('data.json', JSON.stringify(out), function(err) {
 			if (err) throw err;
-			console.log("betfair scraper last run: " + new Date);
-			console.log("Bets with valid returned parameters: " + countBets(out, returned));
+			console.log("Bets within max value: " + countBets(out) + "/" + count);
 		});
 	});
-	//setTimeout(loop, 10000);
+	setTimeout(loop, 10000);
 };loop();
 
-function countBets(data, maxReturn) {
+function countBets(data) {
 	var count = 0;
 	var out = []
 	for (var i=0;i<data.length;i++) {
 		var temp = (data[i][1].split("/")[0])/(data[i][1].split("/")[1]);
-		if (temp<=maxReturn) {
+		if (temp<=returnMax) {
 			count++;
 			out.push(data[i]);
 		}
 	}
-	fs.writeFile('onlyCounted.json', JSON.stringify(out), function(err) {
+	fs.writeFile('validated.json', JSON.stringify(out), function(err) {
 		if (err) throw err;
 	});
 	return count;
 }
-
-
 
 
